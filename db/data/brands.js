571df3dd35b97ab8e32eed23b2458cbee1d50975
David <davidmillett@protonmail.com>
2024-10-08 22:11:11 +0100

    styling

--- db/data/brands.js
+++ db/data/brands.js
@@ -5,6 +5,7 @@ module.exports = [
         established: '1985',
         priceRange: '¥¥¥',
         description: 'KAPITAL takes its name from Kojima, Okayama, an area known as Japan’s ‘Denim Capital’. It reimagines mid-century Americana using heritage production techniques to create an authentic workwear feel. The aesthetic is eclectic and handicraft-inspired.',
+        logo: 'uploads/kapital2.jpeg'
     },
     {
         name: 'Visvim',
@@ -12,5 +13,14 @@ module.exports = [
         established: '2000',
         priceRange: '¥¥¥',
         description: 'Referring to his style as ‘future vintage’, visvim’s designer Hiroki Nakamura aims to create timeless pieces infused with American traditions. Informed by the past and present, the Japanese brand explores new ways to enhance garments with age-old production techniques, from vegetable tanned suede sneakers to rope-dyed selvedge denim.',
+        logo: 'uploads/visvim1.jpg'
+    },
+    {
+        name: 'BAPE',
+        website: 'https://uk.bape.com/',
+        established: '1993',
+        priceRange: '¥¥',
+        description: 'Since bursting on the scene in 1993, Japanese streetwear brand A Bathing Ape, also known as BAPE was founded by Tomoaki Nagao aka DJ Nigo. Discover a merge of popular culture and high fashion for a contemporary clash that redefines streetwear fashion. This season divulges into a selection of bold graphic print pieces including slogan-emblazoned BAPE t-shirts, logo-embossed hoodies and sweatshirts for contemporary casuals.',
+        logo: 'uploads/bape1.png'
     },
 ]
